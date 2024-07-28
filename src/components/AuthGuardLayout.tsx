41e46d268a8ccc11f5abc7ad1d049d64d7031c7b
heybys <byn0174@gmail.com>
2024-07-28 20:33:24 +0900

    add shadcn

--- src/components/AuthGuardLayout.tsx
+++ src/components/AuthGuardLayout.tsx
@@ -3,7 +3,6 @@ import {Outlet} from "react-router-dom";
 const AuthGuardLayout = () => {
   return (
       <>
-        <div>Secure Area</div>
         <Outlet/>
       </>
   )
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,15 @@
+import {useState} from "react";
+import {Button} from "../../components/ui/button";
+
 const HomePage = () => {
+  const [count, setCount] = useState(0);
+
   return (
       <>
         <h1>
           Hello world!
         </h1>
+        <Button onClick={() => setCount(count + 1)}>Easy</Button>
         <header>Home</header>
         <div>Thanks for visiting our site.</div>
       </>
